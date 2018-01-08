483c25b4a6e5ff368327b5a710d749d70f925c42
emailofx@gmail.com <xulong1982>
2018-01-07 19:50:05 -0500

    2nd

--- binary.R
+++
@@ -1,9 +0,0 @@
-x = c(rep("A", 10), rep("B", "15"), rep("C", 20))
-
-x = sample(x)
-
-x = sapply(c("A", "B", "C"), function(xx) as.numeric(x == xx))
-
-summary(lm(x[, 1] ~ x[, 2] + x[, 3]))
-
-y = rnorm(45, mean = 1, sd = 2)
